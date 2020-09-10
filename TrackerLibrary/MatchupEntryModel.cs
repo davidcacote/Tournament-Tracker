adb074fd268d1ea7d4ff697fb88d78ddef33938d
davidcacote <daccr_malluco@msn.com>
2020-09-10 16:50:34 +0100

    Add project files.

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Representa uma equipa num encontro do torneio
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// Representa o resultado do encontro
+        /// </summary>
+        public double Score { get; set; }
+        /// <summary>
+        /// Representa o ultimo encontro desta equipa como vencedora
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
